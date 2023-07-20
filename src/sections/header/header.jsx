b13fa40f00e6ecd3cee0dab019dadd4bb1c3aa59
Vincent <makokhavomondi@gmail.com>
2023-07-20 20:43:39 +0300

    :construction: Create options list on the navbar for enabling various actions
    Actions that would be enabled include viewing posts from followers, seeing user profile, etc

--- src/sections/header/header.jsx
+++ src/sections/header/header.jsx
@@ -1,9 +1,54 @@
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../redux/user";
+import Button from "../../elements/button/button";
 import "./header.css"
+import { useNavigate } from "react-router-dom";
 
 function Header(){
+    const loggedIn = useSelector(state => state.user.loggedIn)
+    const userDetails = useSelector(state => state.user.userDetails)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    function viewLoggedInUserPosts(){
+
+    }
+
+    function viewLoggedInUserDetails(){
+
+    }
+
+    function handleLogoutClick(){
+        dispatch(logout())
+    }
+
+    function viewFollowing(){
+
+    }
+
     return (
         <div id="section-header">
-            <nav></nav>
+            <div className="container">
+                <h1>Socially</h1>
+
+                <div className="actions">
+                    <ul className="menu">
+                        <li onClick={()=>navigate('home')}>Feed</li>
+
+                        {
+                            loggedIn ?
+                                <>
+                                    <li onClick={viewLoggedInUserPosts}>My Posts</li>
+                                    <li onClick={viewFollowing}>Following</li>
+                                    <li onClick={viewLoggedInUserDetails}>{userDetails.name}</li>
+                                    <li><Button text="Logout" action={handleLogoutClick} /></li>
+                                </>
+                            :
+                            <li><Button text="Login" action={()=> navigate('login')}/></li>
+                        }
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
