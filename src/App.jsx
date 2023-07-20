49633af7a77ec8dd8d5af5b6bdd41c3d413660ca
Vincent <makokhavomondi@gmail.com>
2023-07-20 12:31:33 +0300

    :sparkles: Implement react router dom usage in our app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
+import Header from './sections/header/header'
 import Login from './pages/login/login'
+import { Routes, Route } from 'react-router-dom'
+
 
 function App() {
   return (
     <>
-    <Login />
+      <Header />
+      <Routes>
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/" element={<Login />} />
+      </Routes>
     </>
   )
 }
