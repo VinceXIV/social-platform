ebdc4fc92ea3fb506ad571ebccf1e88ebad2e738
Vincent <makokhavomondi@gmail.com>
2023-07-20 22:40:54 +0300

    :recycle: Clean up the posts redux state
    Also enable saving loggedInUserPosts state where the posts by the logged in user are saved

--- src/redux/posts.js
+++ src/redux/posts.js
@@ -15,7 +15,8 @@ if(res.ok){
 // We use the posts we got above to set it as the
 // default state of the posts to be shown to the user
 const initialState = {
-    posts: posts,
+    posts: posts, // These are posts by the users of the social app
+    loggedInUserPosts: [] // posts by the currently logged in user
 }
 
 
@@ -26,33 +27,34 @@ export const counterSlice = createSlice({
 
   reducers: {
     setPosts: (state, data) => {
-        const newPostArray = data.payload
-        state.posts = newPostArray
+        state.posts = data.payload
     },
 
     addPost: (state, data) => {
-        const newPost = data.payload
-        state.posts = state.posts.push(newPost.paylo)
+        state.posts = [...state.posts, data.payload]
     },
 
-    deletePost: (state, deletedPost) => {
-        state.posts = state.posts.filter(post => post.id !== deletedPost.id)
+    deletePost: (state, data) => {
+        state.posts = state.posts.filter(post => post.id !== data.payload.id)
     },
 
     updatePost: (state, data) => {
-        const updatedPost = data.payload
         state.posts = state.posts.map(post => {
-            if(post.id === updatedPost.id){
-                return {...post, ...updatedPost}
+            if(post.id === data.payload.id){
+                return {...post, ...data.payload}
             }else {
                 return post
             }
         })
+    },
+
+    setLoggedInUserPosts: (state, data) => {
+        state.loggedInUserPosts = data.payload
     }
   },
 })
 
 // Action creators are generated for each case reducer function
-export const {setPosts, addPost, deletePost, updatePost } = counterSlice.actions
+export const {setPosts, addPost, deletePost, updatePost, setLoggedInUserPosts } = counterSlice.actions
 
 export default counterSlice.reducer
