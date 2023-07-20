057f08d1914599f4878a0831134adb0651b5f5f5
Vincent <makokhavomondi@gmail.com>
2023-07-20 16:12:29 +0300

    :lipstick: Style the posts

--- src/components/post/post.jsx
+++ src/components/post/post.jsx
@@ -3,8 +3,8 @@ import "./post.css"
 function Post({post}){
     return (
         <div id="component-post" className="component">
-            <h2>{post.title}</h2>
-            <p>{post.body}</p>
+            <h2 className="title">{post.title}</h2>
+            <p className="body">{post.body}</p>
         </div>
     )
 }
