1a89d3ea8dbcbb50c03a6f1d747d1d4328d46198
Vincent <makokhavomondi@gmail.com>
2023-07-20 22:38:03 +0300

    :sparkles: Update the loggedInUserPosts when a user logs ni

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,19 +1,35 @@
 import "./login.css"
 import LoginForm from "../../components/login-form/loginForm";
 import LoginLeft from "../../components/login-left/loginLeft";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { setLoggedInUserPosts } from "../../redux/posts";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import apiHost from "../../utilities/api";
 
 function Login(){
     const navigate = useNavigate()
-    const loggedIn = useSelector(state => state.user.loggedIn)
+    const dispatch = useDispatch()
+    const { loggedIn, userDetails } = useSelector(state => state.user)
 
     // If the person is already logged in, they are redirected
     // to the homepage when they try to access this (login) page
     useEffect(()=>{
         if(loggedIn){
-            navigate('/home')
+            // Save the posts made by the currently logged in user to a state
+            fetch(`${apiHost}/users/${userDetails.id}/posts`)
+            .then(res => {
+                if(res.ok){
+                    res.json().then(data => {
+                        dispatch(setLoggedInUserPosts(data))
+                    })
+                }else {
+                    res.json().then(error => console.warn(error))
+                }
+            })
+            
+            // Finally navigate to home page
+            navigate('/home/feed')
         }
     }, [loggedIn])
 
