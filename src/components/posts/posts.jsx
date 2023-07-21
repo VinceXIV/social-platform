0bfd8dcc4173f540e52ef78d0f53e7281d09d631
Vincent <makokhavomondi@gmail.com>
2023-07-21 23:06:46 +0300

    :construction: Start working on searching posts

--- src/components/posts/posts.jsx
+++ src/components/posts/posts.jsx
@@ -2,9 +2,21 @@ import "./posts.css"
 import Post from "../post/post"
 import { useSelector } from "react-redux";
 import Paywall from "../../components/paywall/paywall";
+import { useState } from "react";
+import Button from "../../elements/button/button";
 
 function Posts({posts = []}){
     const paywalled = useSelector(state => state.paywall.paywalled)
+    const [searchInput, setSearchInput] = useState('')
+
+    function handleSearchInputChange(e){
+        setSearchInput(e.target.value)
+    }
+
+    function searchPost(e){
+        e.preventDefault()
+
+    }
 
     return (
         <div id="section-posts" className="section">
@@ -13,6 +25,12 @@ function Posts({posts = []}){
                 // User is paywalled after viewing all 20 free posts
                 paywalled? <Paywall /> : ''
             }
+
+            <form onSubmit={searchPost}>
+                <label htmlFor="search">Search Post</label>
+                <input name='search' value={searchInput} onChange={handleSearchInputChange} />
+                <Button text="submit" action={searchPost} />
+            </form>
 
             {
                 posts.map(post => {
