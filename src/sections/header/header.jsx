f651628fd15c5e7a6c506952a29362e10735cbc9
Vincent <makokhavomondi@gmail.com>
2023-07-21 15:53:37 +0300

    :sparkles: Show the active page/section in the header section
    Do this by underlining the action name

--- src/sections/header/header.jsx
+++ src/sections/header/header.jsx
@@ -2,13 +2,22 @@ import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../../redux/user";
 import Button from "../../elements/button/button";
 import "./header.css"
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function Header(){
     const loggedIn = useSelector(state => state.user.loggedIn)
     const userDetails = useSelector(state => state.user.userDetails)
     const dispatch = useDispatch()
     const navigate = useNavigate()
+    const location = useLocation()
+
+    function getActiveState(expectedPathName){
+        if(location.pathname === expectedPathName){
+            return 'active'
+        }else {
+            return 'inactive'
+        }
+    }
 
     return (
         <div id="section-header">
@@ -17,14 +26,26 @@ function Header(){
 
                 <div className="actions">
                     <ul className="menu">
-                        <li onClick={()=>navigate('home/feed')}>Feed</li>
+                        <li className={`action ${getActiveState('/home/feed')}`}
+                            onClick={()=>navigate('home/feed')}>
+                            Feed
+                        </li>
 
                         {
                             loggedIn ?
                                 <>
-                                    <li onClick={()=>navigate('home/my-posts')}>My Posts</li>
-                                    <li onClick={()=>navigate('home/following')}>Following</li>
-                                    <li onClick={()=>navigate('home/profile')}>{userDetails.name}</li>
+                                    <li className={`action ${getActiveState('/home/my-posts')}`}
+                                        onClick={()=>navigate('home/my-posts')}>
+                                        My Posts
+                                    </li>
+                                    <li className={`action ${getActiveState('/home/following')}`}
+                                        onClick={()=>navigate('home/following')}>
+                                        Following
+                                    </li>
+                                    <li className={`action ${getActiveState('/home/profile')}`}
+                                        onClick={()=>navigate('home/profile')}>
+                                        {userDetails.name}
+                                    </li>
                                     <li><Button text="Logout" action={()=>dispatch(logout())} /></li>
                                 </>
                             :
