c4381db5ab10e93a5acd77f1542b3b27b8ecc858
Vincent <makokhavomondi@gmail.com>
2023-07-22 02:40:45 +0300

    :lipstick: Button, header, etc

--- src/sections/feed/feed.jsx
+++ src/sections/feed/feed.jsx
@@ -1,7 +1,5 @@
 import { useSelector } from "react-redux";
-import { useGet } from "../../utilities/hooks";
 import "./feed.css"
-import apiHost from "../../utilities/api";
 import Posts from "../../components/posts/posts";
 
 function Feed(){   
