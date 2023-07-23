b1efc53781e6674c2be467330934f50e8802c7ce
Vincent <makokhavomondi@gmail.com>
2023-07-23 19:51:09 +0300

    :adhesive_bandage: Prevent the posts in feed section from loading more than once

--- src/sections/feed/feed.jsx
+++ src/sections/feed/feed.jsx
@@ -1,6 +1,6 @@
 import "./feed.css"
 import Posts from "../../components/posts/posts";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useGet } from "../../utilities/hooks";
 import apiHost from "../../utilities/api";
 import { setPosts } from "../../redux/posts";
@@ -8,10 +8,18 @@ import { useEffect } from "react";
 
 function Feed(){   
     const [feedPosts] = useGet(`${apiHost}/posts`)
+    const posts = useSelector(state => state.posts.posts)
     const dispatch = useDispatch()
 
     useEffect(()=>{
-        dispatch(setPosts(feedPosts))
+        // Other sections might have modified the posts shown.
+        // For instance, the following section wil only limit
+        // the posts shown to users the current logged in user
+        // is following. As such, the posts state is likely to
+        // contain less than the maximum number of posts (100)
+        if(!!posts && posts.length < 100){
+            dispatch(setPosts(feedPosts))
+        }
     }, [feedPosts])
 
     return (
