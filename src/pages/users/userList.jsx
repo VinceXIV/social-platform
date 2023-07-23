ea98d58317fb546d0479400c24526470abfc8108
Vincent <makokhavomondi@gmail.com>
2023-07-23 16:40:28 +0300

    :sparkles: Show paywall on user list when a regular user attempts to block another

--- src/pages/users/userList.jsx
+++ src/pages/users/userList.jsx
@@ -1,9 +1,18 @@
+import { useSelector } from "react-redux";
 import AppUsers from "../../components/app-users/app-users"
+import Paywall from "../../components/paywall/paywall";
 import "./userList.css"
 
 function UserList(){
+    const paywalled = useSelector(state => state.paywall.paywalled)
+    
     return (
         <div id="page-user-list" className="page">
+            {
+                // Show the paywall if the user is paywalled
+                // User is paywalled after viewing all 20 free posts
+                paywalled? <Paywall /> : ''
+            }
             <div className="container">
                 <div className="scroll">
                     <AppUsers />
